471a085fe409d2ea0f6b9f9ea3d26500f85c6130
vivek-Reddy333 <vivekgarlapti15103@gmail.com>
2022-05-30 08:53:02 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,12 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Susan',
+      lastName: 'Jordon',
+      email: 'susan@example.com',
+      task: 'Read Harry Potter 1',
+      date: '2019-04-11'
+    }
+  ];
+  
+  export { employeesData };
